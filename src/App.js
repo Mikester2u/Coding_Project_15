2288b989a18e894c04b7885c9d4c5017eeb8b169
Michael Rueda <michael.a.rueda22@gmail.com>
2024-07-05 18:33:03 -0400

    Add personal information section with styled components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const PersonalInfo = () => { //Personal Information Section
+    return (
+      <section className="personal-info">
+        <h2>Personal Information</h2>
+        <p>Name: Michael Rueda</p>
+        <p>Email: michael.a.rueda22@gmail.com</p>
+        <p>Biography: Senior at the University of South Florida looking to advance in the world</p>
+        <p>Linkedin: https://www.linkedin.com/in/michael-rueda-thehomedepot/</p>
+      </section>
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
